bccc2d785659c7f84d69f509a9ce339b45b5e355
Kasra Karami <Kasra_K2K@yahoo.com>
2023-04-30 19:52:44 +0330

    adding two custom metrics
    Co-Authored-By: kasrakaramikk <122204823+kasrakaramikk@users.noreply.github.com>
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>

--- metrics.js
+++ metrics.js
@@ -5,6 +5,18 @@ const port = process.env.PORT || 9100
 
 app.use(express.json())
 
+export const restResponseTimeHistogram = new client.Histogram({
+  name: "rest_response_time_duration_seconds",
+  help: "REST API response time in seconds.",
+  labelNames: ["method", "route", "status_code"],
+})
+
+export const databaseResponseTimeHistogram = new client.Histogram({
+  name: "db_response_time_duration_seconds",
+  help: "Database response time in seconds.",
+  labelNames: ["operation", "success"],
+})
+
 export const startMetricsServer = () => {
   collectDefaultMetrics()
 
